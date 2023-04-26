1d6aa83253347b7d99a35cccbcb16aa81edd4b2b
RezaB <90reza@gmail.com>
2023-04-26 01:58:40 -0600

    latent generator and bash call

--- configs/in256/vit-b_layer12_lr1e-4_099_099_pred_x0__min_snr_5__fp16_bs8x32.sh
+++ configs/in256/vit-b_layer12_lr1e-4_099_099_pred_x0__min_snr_5__fp16_bs8x32.sh
@@ -1,9 +1,21 @@
+#!/bin/sh
+#SBATCH -p gpu-v100
+#SBATCH --job-name=Diffusion
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=8
+#SBATCH -t 1-00:00:00
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:1
+## #SBATCH --gres=gpu:p100:3
+#SBATCH -o %x-%j.out
+
 # set -ex
 
-pip install -r requirements.txt
-pip install -e .
+# pip install -r requirements.txt
+# pip install -e .
 
-DATA_DIR="../datasets/ILSVRC2012_ldm_256_diffuser/train/"
+DATA_DIR="/home/rbasiri/Dataset/GAN/train_foot/train/"
 
 GPUS=$1
 BATCH_PER_GPU=$2
@@ -11,17 +23,17 @@ EXP_NAME=vit-b_layer12_lr1e-4_099_099_pred_x0__min_snr_5__fp16_bs${GPUS}x${BATCH
 
 MODEL_BLOB="/mnt/external"
 if [ ! -d $MODEL_BLOB ]; then
-    MODEL_BLOB="."
+    MODEL_BLOB="/home/rbasiri/Dataset/saved_models/Diffusion"
 fi
 
 OPENAI_LOGDIR="${MODEL_BLOB}/exp/guided_diffusion/$EXP_NAME"
 # if permission denied
-sudo mkdir -p $OPENAI_LOGDIR && sudo chmod 777 $OPENAI_LOGDIR
+mkdir -p $OPENAI_LOGDIR
 OPENAI_LOGDIR=$OPENAI_LOGDIR \
-    torchrun --nproc_per_node=${GPUS} --master_port=23456 scripts_vit/image_train_vit.py \
-    --data_dir $DATA_DIR --image_size 32 --class_cond True --diffusion_steps 1000 \
+    torchrun --nproc_per_node=1 --master_port=23456 scripts_vit/image_train_vit.py \
+    --data_dir $DATA_DIR --image_size 32 --class_cond False --diffusion_steps 1000 \
     --noise_schedule cosine --rescale_learned_sigmas False \
-    --lr 1e-4 --batch_size ${BATCH_PER_GPU} --log_interval 10 --beta1 0.99 --beta2 0.99 \
+    --lr 1e-4 --batch_size 2 --log_interval 10 --beta1 0.99 --beta2 0.99 \
     --exp_name $EXP_NAME --use_fp16 True --weight_decay 0.03 \
     --use_wandb False --model_name vit_base_patch2_32 --depth 12 \
     --predict_xstart True --warmup_steps 0 --lr_anneal_steps 0 \
---
+++ laten_img_generator.py
@@ -0,0 +1,61 @@
+import torch
+  ## Imaging  library 
+from PIL import Image 
+from torchvision import transforms as tfms  
+## Basic libraries 
+import numpy as np 
+#%matplotlib inline  
+## Loading a VAE model 
+from diffusers import AutoencoderKL 
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", torch_dtype=torch.float16).to("cuda")
+
+def load_image(p):
+   '''     
+   Function to load images from a defined path     
+   '''    
+   return Image.open(p).convert('RGB').resize((256,256))
+def pil_to_latents(image):
+    '''     
+    Function to convert image to latents     
+    '''     
+    init_image = tfms.ToTensor()(image).unsqueeze(0) * 2.0 - 1.0   
+    init_image = init_image.to(device="cuda", dtype=torch.float16)
+    init_latent_dist = vae.encode(init_image).latent_dist.sample() * 0.18215     
+    return init_latent_dist  
+def latents_to_pil(latents):     
+    '''     
+    Function to convert latents to images     
+    '''     
+    latents = (1 / 0.18215) * latents     
+    with torch.no_grad():         
+        image = vae.decode(latents).sample     
+    
+    image = (image / 2 + 0.5).clamp(0, 1)     
+    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()      
+    images = (image * 255).round().astype("uint8")     
+    pil_images = [Image.fromarray(image) for image in images]        
+    return pil_images
+
+import os
+
+# specify the img directory path
+path = "/home/rbasiri/Dataset/GAN/test/DFU/"
+savepath = "/home/rbasiri/Dataset/GAN/test/DFU/latent"
+
+# list files in img directory
+files = os.listdir(path)
+
+for file in files:
+    # make sure file is an image
+    if file.endswith(('.jpg', '.png', 'jpeg')):
+        img_path = path + file
+
+        # load file as image...
+        img = load_image(img_path)
+        latent_img = pil_to_latents(img)
+        latent_save_path = os.path.join(savepath, file[:-4]+'.npy')
+        np.save(latent_save_path, latent_img.cpu().detach().numpy())
+
+# latent_save_path = os.path.join(test_opts.exp_dir, 'latent_code_%05d.npy'%global_i)
+# latent_save_path = os.path.join(savepath, 'test.npy')
+# np.save(latent_save_path, latent_img.cpu().numpy())
